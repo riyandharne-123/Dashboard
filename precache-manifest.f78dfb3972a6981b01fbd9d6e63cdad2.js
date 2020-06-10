3f69b748b929d066b4198202c639100d9726bb8b
riyandharne-123 <rockydods123@gmail.com>
2020-06-10 07:44:53 +0530

    Updates

---
+++ precache-manifest.f78dfb3972a6981b01fbd9d6e63cdad2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ef508040ffbfc93eb9b72d0d1e94f2d",
+    "url": "/Dashboard/index.html"
+  },
+  {
+    "revision": "f1ac6b0cae70168a58b5",
+    "url": "/Dashboard/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "14568b24801416e3f2ba",
+    "url": "/Dashboard/static/js/2.96a2b6ca.chunk.js"
+  },
+  {
+    "revision": "23ec6c7f0411a6141095a6c739b32980",
+    "url": "/Dashboard/static/js/2.96a2b6ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1ac6b0cae70168a58b5",
+    "url": "/Dashboard/static/js/main.4da08ce9.chunk.js"
+  },
+  {
+    "revision": "31497257d734095153ea",
+    "url": "/Dashboard/static/js/runtime-main.d31fb66c.js"
+  }
+]);
